e7699ebf2d56487f5e2b3562f247296deb5a06f7
mgalalm <squareno2@gmail.com>
2022-05-03 06:54:14 +0100

    CI & CD

--- src/main.go
+++ src/main.go
@@ -1,8 +1,9 @@
 package main
 import "time"
 func Main(args map[string]interface{}) map[string]interface{} {
-        time.Sleep(20 * time.Second) 
+    time.Sleep(20 * time.Second) 
 	msg := make(map[string]interface{})
-	msg["message"] = "Hello World Nanos"
+	msg["message"] = "Hello World Openwhisk"
+	msg["version"] = 1.0
 	return msg
 }
